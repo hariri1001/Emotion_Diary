c90041b22029ec7009601c547f9554a2734a3c47
leehari <leehariri1001@gmail.com>
2025-01-14 17:44:41 +0900

    Diary 추가기능 완성

--- src/pages/New.jsx
+++ src/pages/New.jsx
@@ -1,14 +1,26 @@
 import Header from "../components/Header"
 import Button from "../components/Button"
-import Editor from "../components/Editor";
+import Editor from "../components/Editor"
+import {useNavigate} from "react-router-dom"
+import {useContext} from "react"
+import {DiaryDispatchContext} from "../App"
 
 const New = () => {
+    const {onCreate} = useContext(DiaryDispatchContext)
+    const nav = useNavigate()
+
+    const onSubmit = (input) => {
+        onCreate(input.createdDate.getTime(), input.emotionId, input.content)
+        nav('/', {replace: true})//뒤로가기를 방지하면서 페이지를 이동시키는 옵션
+    }
+
+
     return (
         <div>
             <Header 
             title={"새 일기 쓰기"} 
-            leftChild={<Button text={"< 뒤로가기"}/>} />
-            <Editor/>
+            leftChild={<Button onClick={() => nav(-1)} text={"< 뒤로가기"}/>} />
+            <Editor onSubmit={onSubmit}/>
         </div>
     )
 };
